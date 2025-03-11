28bf34c0e8943fcf2cc9bbd67080a3d8977d93e0
Marco <mshendriks03@gmail.com>
2025-03-11 11:41:26 +0100

    feat: add protected page (wip)

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,8 @@
 <?php
+require_once 'config.php';
+
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
+    echo "Welcome to the member's area, " . htmlspecialchars($_SESSION['username']) . "!";
+} else {
+    echo "Please log in first to see this page.";
+}
